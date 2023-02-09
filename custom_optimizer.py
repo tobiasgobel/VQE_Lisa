9f31de0e715160175c84d1882277cf1b0f54b91f
Tobias Gobel <tobiasgobel@gmail.com>
2023-02-09 12:13:05 +0100

    opt methods

--- custom_optimizer.py
+++ custom_optimizer.py
@@ -1,6 +1,8 @@
 import scipy
 import numpy as np
+import matplotlib.pyplot as plt
 
+#Optimization class for optizing inside the K-cell
 class E_optimizer:
     def __init__(self, func, x0, args = (), boundary = "hypersphere", epsilon= 1e-4, method = "SLSQP"):
         self.func = func
@@ -9,6 +11,7 @@ class E_optimizer:
         self.epsilon = epsilon
         self.args = args
         self.method = method
+        self.plot = False
 
     def callback(self, x, *args):
         if self.boundary == "hypersphere":
@@ -25,5 +28,46 @@ class E_optimizer:
 
     def optim(self):
         con = {'type':"ineq", 'fun':self.constraint}
-        opt = scipy.optimize.minimize(self.func, self.x0, jac = False,args = self.args, constraints = con, method = self.method)
+        opt = scipy.optimize.minimize(self.func, self.x0, jac = False,args = self.args, constraints = con, method = self.method, options={'rhobeg':0.001, 'disp':True})
         return opt
+
+
+#Optimization class for optimizing between theta_a and theta_t
+class E_optim_cirq:
+    def __init__(self, func, x0, args = (), method = "COBYLA", plot=False):
+        self.func = func
+        self.x0 = np.asarray(x0)
+        self.args = args
+        self.method = method
+        self.plot = plot
+        self.angles = []
+        self.energy = []
+        
+    def callback(self, x):
+        self.angles.append(x)
+        self.energy.append(self.func(x, *self.args))
+
+    def optim(self):
+        opt = scipy.optimize.minimize(self.func, self.x0,args = self.args, method = self.method, callback = self.callback)
+        if self.plot:
+            iterations = len(self.angles)
+            print(self.angles)
+            self.angles = np.array(self.angles).reshape(iterations, len(self.x0))
+            plt.ylabel("Angle")
+            plt.xlabel("Iteration")
+            plt.plot(self.angles)
+            plt.savefig("angles.png")
+            plt.clf()
+
+            plt.ylabel("Log(Energy)")
+            plt.xlabel("Iteration")
+            plt.plot(np.log(self.energy))
+            plt.savefig("Energy.png")
+            plt.clf()
+
+        return opt
+
+
+
+    
+
